c4b39cc790b2d84e638baec7b24b6029e7ad1b78
NiceDrop <30200976+NiceDrop@users.noreply.github.com>
2019-11-12 09:52:32 -0500

    Add README and tweak main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
-import selectDeselectCard from "./assets/js/manageCards.js";
 import exchangeCards from "./assets/js/exchangeCards.js";
 import buy, { hold } from "./assets/js/buyHold.js";
 import cutForDeal, { cutForDealBtn } from "./assets/js/cutForDeal.js";
-import { extraCard } from "./assets/js/deal.js";
 import addHuman, {
   removeHuman,
   addAnimal,
@@ -10,22 +8,16 @@ import addHuman, {
   textPlayerName
 } from "./assets/js/addPlayers.js";
 
-//> Event Listeners
 const addHumanBtn = document.querySelector(".addHumanBtn");
 const removeHumanBtn = document.querySelector(".removeHumanBtn");
 const addAnimalBtn = document.querySelector(".addAnimalBtn");
 const removeAnimalBtn = document.querySelector(".removeAnimalBtn");
-
 const exchangeBtn = document.querySelector(".exchangeBtn");
 const buyBtn = document.querySelector(".buyBtn");
 const holdBtn = document.querySelector(".holdBtn");
 
-//* Selects/deselects extra hand's and current player's cards on screen
-//* ...by adding an event listener to the extraCard NodeList...
-extraCard.forEach((card) => card.addEventListener("click", selectDeselectCard));
-
-//* Wrapped event listeners in function to avoid "Test suite failed to run"
-//* TypeError: Cannot read property 'addEventListener' of null
+//? Wrapped event listeners in function to avoid "Test suite failed to run"
+//? TypeError: Cannot read property 'addEventListener' of null
 const addListeners = () => {
   //* Add or remove players
   textPlayerName.addEventListener("submit", addHuman);
